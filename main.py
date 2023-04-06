75c1ff1c02b11f573b215367911f8c2b27d3eea7
Bravecowcow <86814394+Bravecowcow@users.noreply.github.com>
2023-04-06 10:27:25 +0800

    test4

--- main.py
+++ main.py
@@ -24,11 +24,11 @@ birthdays = os.environ["BIRTHDAY"].split(',')
 
 # 获取天气和温度
 def get_weather(city):
-#     url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
-#     res = requests.get(url).json()
-#     weather = res['date']['list'][0]
-#     return weather['weather'], math.floor(weather['temp'])
-    return '晴', '15℃'
+    url = 'http://wthrcdn.etouch.cn/weather_mini'
+    response = requests.get(url, {'city': name})
+    result = json.loads(response.content.decode())
+    
+    return result.get('data').get('today').get('type'), result.get('data').get('today').get('low')
 
 
 # 当前城市、日期
