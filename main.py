c5499949ac2449005ababad28b3a414e5c33c769
Bravecowcow <86814394+Bravecowcow@users.noreply.github.com>
2023-04-06 09:56:14 +0800

    test

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ birthdays = os.environ["BIRTHDAY"].split(',')
 def get_weather(city):
     url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
     res = requests.get(url).json()
-    weather = res['data']['list'][0]
+    weather = res['date']['list'][0]
     return weather['weather'], math.floor(weather['temp'])
 
 
